a168ac496540fca6dfa5148f764592281a58829c
Anton <santon84@gmail.com>
2023-01-25 14:43:49 +0300

    ie11

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
+import 'babel-polyfill';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
