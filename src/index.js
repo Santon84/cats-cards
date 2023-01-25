e47ed98175d9bfb4e3ccad846b72eeedd05abfaf
Anton <santon84@gmail.com>
2023-01-25 14:47:01 +0300

    ie11

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
-import 'babel-polyfill';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
